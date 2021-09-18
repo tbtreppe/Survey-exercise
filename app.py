4b39fb9019888c9696859b536678b991f0be3c06
Tracey <ttrepper@yahoo.com>
2021-09-18 16:46:40 -0400

    added sessions

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.config['SECRET_KEY']= "secret!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False 
 debug = DebugToolbarExtension(app)
 
-responses = []
+RESPONSE_KEY = "responses"
 
 @app.route("/")
 def home_page():
@@ -15,13 +15,15 @@ def home_page():
 @app.route("/begin", methods=["POST"])
 def start_survey():
 
-  
+    session[RESPONSE_KEY] = []
     return redirect("/questions/0")
 
 @app.route("/answer",  methods=["POST"])
 def questions():
     choice = request.form['answer']
+    responses = session[RESPONSE_KEY]
     responses.append(choice)
+    session[RESPONSE_KEY] = responses
 
     if (len(responses)==len(survey.questions)):
         return redirect("/thankyou")
@@ -30,6 +32,7 @@ def questions():
 
 @app.route("/questions/<int:qid>")
 def show_question(qid):
+    responses = session[RESPONSE_KEY]
 
     if (responses is None):
         
