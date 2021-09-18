7da2c5349dde0531d1068a36d8b1804614142125
Tracey <ttrepper@yahoo.com>
2021-09-18 16:33:35 -0400

    finished exercise

--- app.py
+++ app.py
@@ -1,24 +1,56 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 app = Flask(__name__)
-app.config['SECRET_KEY']= "name"
+app.config['SECRET_KEY']= "secret!"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False 
 debug = DebugToolbarExtension(app)
 
 responses = []
 
-@app.route('/homepage')
+@app.route("/")
 def home_page():
-    return render_template("homepage.html", survey=survey)
+    return render_template("survey_start.html", survey=survey)
 
-@app.route('/questions/0')
+@app.route("/begin", methods=["POST"])
+def start_survey():
+
+  
+    return redirect("/questions/0")
+
+@app.route("/answer",  methods=["POST"])
 def questions():
-    return render_template("questions0.html")
+    choice = request.form['answer']
+    responses.append(choice)
 
-@app.route('/answers')
-def answers():
-    return responses.append("answers")
+    if (len(responses)==len(survey.questions)):
+        return redirect("/thankyou")
+    else:
+        return redirect(f"/questions/{len(responses)}")
 
-@app.route('/')
-def redirect_to_next_question():
-    return redirect("questions1.html") 
+@app.route("/questions/<int:qid>")
+def show_question(qid):
+
+    if (responses is None):
+        
+        return redirect("/")
+
+    if (len(responses) == len(survey.questions)):
+        
+        return redirect("/thankyou")
+
+    if (len(responses) != qid):
+        
+        flash(f"Invalid question id: {qid}.")
+        return redirect(f"/questions/{len(responses)}")
+
+    question = survey.questions[qid]
+    return render_template(
+        "questions.html", question_num=qid, question=question)
+
+
+
+@app.route("/thankyou")
+def thankyou():
+    return render_template("thankyou.html")
+
